a222adc43aff43c568b749ae907f79084d8774af
Xiaoxuan Wang <xiaoxuanwang@microsoft.com>
2025-03-19 11:22:13 +0800

    demo
    Signed-off-by: Xiaoxuan Wang <xiaoxuanwang@microsoft.com>

--- cmd/oras/internal/display/metadata/model/discover.go
+++ cmd/oras/internal/display/metadata/model/discover.go
@@ -32,7 +32,7 @@ type Discover struct {
 // Node represents a node in the discovered reference tree.
 type Node struct {
 	Descriptor
-	Referrers []*Node `json:"manifests,omitempty"`
+	Referrers []*Node `json:"referrers"`
 }
 
 // Add adds a node to the discovered referrers tree.
@@ -64,5 +64,4 @@ func NewNode(name string, desc ocispec.Descriptor) *Node {
 	return &Node{
 		Descriptor: FromDescriptor(name, desc),
 	}
-
 }
